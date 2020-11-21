a94927521057cfb77e07b156bc28a1cec832b33f
jmarquez30 <jmarquez.30@hotmail.com>
2020-11-21 13:29:43 -0600

    guardar cambios

---
+++ 01listas.py
@@ -0,0 +1,19 @@
+#archivo explicando listas = arreglo
+
+numeros =  [23, 89, 127, 1289, 8912]
+
+print(numeros)
+
+#para agregar un elemento a una lista ocupamos append
+
+numero_extra = 33
+numero_extra_dos = 1233
+numero_extra_tres = 89
+
+numeros.append(numero_extra)
+print(numeros)
+
+#para eliminar el ultimo elemento de una lista ocupamos pop
+
+numeros.pop()
+print(numeros)
